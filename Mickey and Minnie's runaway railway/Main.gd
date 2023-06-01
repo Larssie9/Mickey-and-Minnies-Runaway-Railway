7c6a4f5c6316b145213bdc56fc7848cea11432ef
Larssie9 <133204139+Larssie9@users.noreply.github.com>
2023-06-01 14:16:50 +0200

    Add files via upload

--- Mickey and Minnie's runaway railway/Main.gd
+++ Mickey and Minnie's runaway railway/Main.gd
@@ -8,11 +8,10 @@ var groen = preload("res://Media/Groen.png")
 var t1 = 0
 var _golaad1 = true
 var _gouitlaad1 = true
-var offset1 = 0
 var _loadbeugels = false
 var _poortjes = true
 var _station = true
-var _uitstation = false
+var _uitstation = true
 var _unloadbeugels = false
 
 func _on_SetupTimer_timeout():
@@ -38,13 +37,7 @@ func _on_LoadVrijgeven2_button_up():
 
 
 func _on_LoadBeugels_pressed():
-	print("LoadBeugels")
 	_loadbeugels = true
-	$Path2D/T1V1/V1.frame = 1
-	$Path2D/T1V2/V2.frame = 1
-	$Path2D/T1V3/V3.frame = 1
-	$Path2D/T1V4/V4.frame = 1
-
 
 func _on_LoadPoortjes_pressed():
 	if poortjes == "dicht":
@@ -84,12 +77,8 @@ func _on_Button_pressed():
 	$Path2D.visible=false
 
 func _on_UnloadBeugels_pressed():
-	print("LoadBeugels")
 	_unloadbeugels = true
-	$Path2D/T1V1/V1.frame = 0
-	$Path2D/T1V2/V2.frame = 0
-	$Path2D/T1V3/V3.frame = 0
-	$Path2D/T1V4/V4.frame = 0
+
 func _on_UnloadVrijgeven1_button_down():
 	print("LoadVrijgeven1")
 	$Map/UnloadVrijgeven2.texture_normal = groenl
@@ -108,30 +97,6 @@ func _on_UnloadVrijgeven2_button_up():
 	print("LoadVrijgeven2")
 	$Map/UnloadVrijgeven1.texture_normal = groen
 
-func _process(delta):
-	if _golaad1 &&  _gouitlaad1:
-		t1 += delta
-		offset1 = t1 * 50
-		if offset1 >= 3710:
-			t1 = 0
-	$Path2D/T1V1.offset = offset1
-	$Path2D/T1V2.offset = offset1 + 45
-	$Path2D/T1V3.offset = offset1 + 90
-	$Path2D/T1V4.offset = offset1 + 135
-	$Path2D/T1VL.offset = offset1 + 180
-	if (((offset1 >= 72)&&(offset1 <= 82))&&_station):
-		_loadbeugels = false
-		_station = false
-		_golaad1 = false
-	if ((offset1 >= 92)&&(offset1 <= 102)):
-		_station = true
-	if (((offset1 >= 3000)&&(offset1 <= 3010))&&_station):
-		_unloadbeugels = false
-		_uitstation = false
-		_gouitlaad1 = false
-	if ((offset1 >= 3020)&&(offset1 <= 3030)):
-		_uitstation = true
-
 func _vrijgeven():
 	if (_loadbeugels && _poortjes):
 		_golaad1 = true
@@ -139,4 +104,3 @@ func _vrijgeven():
 func _unloadvrijgeven():
 	if _unloadbeugels:
 		_gouitlaad1 = true
-	
